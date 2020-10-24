def09c4e7fb2b1d1cfe582493862934b6685c41e
Nikita Gupta <gupniki@amazon.com>
2020-10-24 21:34:14 +0530

    Added case schema.

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      description
+      amazon_wishlist_url
+      address
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      description
+      amazon_wishlist_url
+      address
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      description
+      amazon_wishlist_url
+      address
+      createdAt
+      updatedAt
+    }
+  }
+`;
